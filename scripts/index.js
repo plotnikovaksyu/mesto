dc26c956083d168dad5aeecaaf04f7ba4504daf6
Kseniya Plotnikova <plotnikova-ksyuh@yandex.ru>
2022-11-19 15:48:46 +0300

    fixed bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,23 @@
-// открытие и закрытие попапа 
+// определние переменных //
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupSubmitButtonElement = popupElement.querySelector('.popup__submit-button');
 
 const popapOpenButtonElement = document.querySelector('.profile__edit-button');
 
+const formElement = popupElement.querySelector('.popup__form');
+
+const nameInput = popupElement.querySelector('.popup__input_name');
+const discriptionInput = popupElement.querySelector('.popup__input_discription');
+const profileName = document.querySelector('.profile__title');
+const profileDiscription = document.querySelector('.profile__discription');
+
+
+// открытие и закрытие попапа 
 const openPopup = function () {
     popupElement.classList.add('popup_is-opened');
+    nameInput.value = profileName.textContent;
+    discriptionInput.value = profileDiscription.textContent;
 }
 
 const closePopup = function () {
@@ -26,43 +37,27 @@ popapOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOverlay);
 
-// сохранение и изменения текста в редактируемых полях
-
-const formElement = popupElement.querySelector('.popup__form');
-
-const nameInput = popupElement.querySelector('.popup__edit-name');
-const discriptionInput = popupElement.querySelector('.popup__edit-discription');
-const profileName = document.querySelector('.profile__title');
-const profileDiscription = document.querySelector('.profile__discription');
-
-
-
-popupElement.querySelector('input[name="name"]').value = profileName.textContent;
-popupElement.querySelector('input[name="discription"]').value = profileDiscription.textContent;
 // Обработчик «отправки» формы
- 
-function formSubmitHandler (evt) {
+
+function formSubmitHandler(evt) {
     evt.preventDefault(); //отмена дефолтного поведения, чтобы браузер не перезагрузился
     profileName.textContent = nameInput.value;
     profileDiscription.textContent = discriptionInput.value;
     closePopup();
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
 
 
 //поставить и убрать лайк только для первого элемента
 
-let likeButtonElement = document.querySelector('.grid__button-img');
+const likeButtonElement = document.querySelector('.grid__button');
 
-console.log(likeButtonElement.length);
+console.log(likeButtonElement);
 
 const getLike = function () {
-  likeButtonElement.classList.toggle('grid__fill');
+    likeButtonElement.classList.toggle('grid__fill');
 }
 
 //обработчик сбытий
-
-likeButtonElement.addEventListener ('click', getLike);
-
-
+likeButtonElement.addEventListener('click', getLike);
